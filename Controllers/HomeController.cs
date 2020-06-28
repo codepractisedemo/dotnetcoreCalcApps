fc7add5a6e91ee6b37df8e1785fc4c8162736efd
Jagan Rajagopal <askjaganr@gmail.com>
2020-06-28 21:21:35 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnetcoreCalcApps.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+
+        public IActionResult sum()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult add()
+        {
+            int num1 = Convert.ToInt32(HttpContext.Request.Form["txtFirst"].ToString());
+            int num2 = Convert.ToInt32(HttpContext.Request.Form["txtSecond"].ToString());
+            int result = num1 + num2;
+            ViewBag.SumResult = result.ToString();
+            return View("sum");
+        }
+    
+    }
+}
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace dotnetcoreCalcApps.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            String datetime = DateTime.Now.ToString();
+            ViewData["TimeData"] = datetime;
+        }
+    }
+}
